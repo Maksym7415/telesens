69a0c18b51f879435ac1b94a717aa27e11247ef4
Maksym7415 <popovmaksim7415@gmail.com>
2020-01-27 10:34:34 +0200

    project is ready

--- src/app/rootPage/components/content.js
+++ src/app/rootPage/components/content.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SongCard from './songCard'
 import { actionAdd } from '../../../redux/reducers/synchroReducer'
-import { dive } from '../../../functions'
 import { connect } from 'react-redux'
 
-const Content = props =>
-  <div className= 'content'>
-    <span> {props.name} </span>
-    {props.content.map(item => <SongCard onClick= {() => props.add(item)} key= {item.contentNo} catId= {item.contentCatId} id= {item.contentNo} src= {item.imageId} title= {item.title} artist= {item.artist} price= {item.priceModelNo + '$'}/>)}
-  </div>
+const Content = props => {
+
+  let [progressStyle, setProgressStyle] = useState('0%')
+  let [position, setPosition] = useState()
+  let [audio, setAudio] = useState(new Audio)
+  let [currentTime, setCurrentTime] = useState(new Audio().currentTime)
+  let [int, setInt] = useState()
+
+    let progress = 0
+    let changeProgress = () => {
+      setInt(int = setInterval(() =>{
+        setCurrentTime(currentTime = audio.currentTime)
+        progress = Math.round(currentTime)*100/Math.round(audio.duration)
+        setProgressStyle(progressStyle = `${progress}%`)
+        console.log(audio.currentTime)
+      }, 500))
+    }
+
+    const handlePlay = (id) => {
+      if (currentTime === 0) {
+        setPosition(position = id)
+        audio.src = `https://t-rbt.telesens.ua/t-rbt/sound?id=${id}&type=public`
+        audio.play()
+        changeProgress()
+      } else {
+        clearInterval(int)
+        setCurrentTime(currentTime = 0)
+        setProgressStyle(progressStyle = '0%')
+        audio.pause()
+        console.log(currentTime)
+      }
+    }
+
+    audio.onended = () => {
+      clearInterval(int)
+      setProgressStyle(progressStyle = '0%')
+      setPosition(position = null)
+      setCurrentTime(currentTime = new Audio().currentTime)
+    }
+  return (
+    <div className= 'content'>
+      {props.content.map(item => <SongCard playSong= {() => handlePlay(item.contentNo)} progressStyle = {{margin: '0', backgroundColor: 'blue', width: progressStyle}} progressClass= {position === item.contentNo && currentTime !== 0 ? 'activeSong' : 'unActiveSong'} onClick= {() => props.add(item)} key= {item.contentNo} catId= {item.contentCatId} id= {item.contentNo} src= {item.imageId} title= {item.title} artist= {item.artist} price= {item.priceModelNo + '$'}/>)}
+    </div>
+  )
+}
 
 export default connect(null, {add: actionAdd})(Content)
--- src/app/rootPage/components/songCard/index.js
+++ src/app/rootPage/components/songCard/index.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const SongCard = props =>
-  <div className= 'song-card'>
-    <img alt='logo' src= {`https://t-rbt.telesens.ua/t-rbt/image?id=${props.src}`}/>
-    <span> {props.title} </span>
-    <span> {props.artist} </span>
-    <div>
-      <span> {props.price} </span>
-      <Link onClick= {props.onClick} to= {`/buy/${props.id}/${props.catId}`}><i className="fas fa-shopping-cart"></i></Link>
-      <Link to= '/gift'> <i className="fas fa-gift"></i> </Link>
-      <Link to= {`/info/${props.id}/${props.catId}`}> <i className="fas fa-info-circle"></i> </Link>
+const SongCard = props => {
+
+
+  return (
+    <div className= 'song-card'>
+      <img onClick= {props.playSong} alt='logo' src= {`https://t-rbt.telesens.ua/t-rbt/image?id=${props.src}`}/>
+      <div className= {props.progressClass}>
+        <div  style= {props.progressStyle}></div>
+      </div>
+      <span> {props.title} </span>
+      <span> {props.artist} </span>
+      <div>
+        <span> {props.price} </span>
+        <Link onClick= {props.onClick} to= {`/buy/${props.id}/${props.catId}`}><i className="fas fa-shopping-cart"></i></Link>
+        <Link to= '/gift'> <i className="fas fa-gift"></i> </Link>
+        <Link to= {`/info/${props.id}/${props.catId}`}> <i className="fas fa-info-circle"></i> </Link>
+      </div>
     </div>
-  </div>
+  )
+}
 
 export default SongCard
